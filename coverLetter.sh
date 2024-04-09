916ee5f9e1e953cca688a7e8e3009975d3b7460f
Andrii Bohaichuk <bogaychuk.a@gmail.com>
2024-04-09 15:50:48 +0300

    changed phone number

---
+++ coverLetter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'I am writing to express my interest in the Senior Java Developer position at' $1', as advertised on your company website. With a strong background in Java development and a proven track record of delivering high-quality software solutions. I am confident in my ability to contribute effectively to your team and help drive your company success.'
+
+echo 'I am well-versed in a range of Java technologies, including Spring Framework, Hibernate, and Gradle, and I am adept at solving complex problems with innovative and efficient solutions. My experience also extends to integrating third-party APIs, optimizing database performance, and ensuring the security and reliability of applications.'
+
+echo 'I am excited about the opportunity to contribute my skills and experience to '$1'. Thank you for considering my application. I look forward to the possibility of discussing how my background aligns with your needs and goals.'
+
